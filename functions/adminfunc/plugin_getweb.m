AminAlam/eeglab

commit 895e90ba208b363706d7632402866a57badca993

bug cuased by 1baff0b9db363c7cf9e51834513ed8a1ce54631c is resolved

functions/adminfunc/plugin_getweb.m
@@ -92,7 +92,7 @@ function [plugin,eeglabVersionStatus] = plugin_getweb(type, pluginOri, varargin)
                 return
             end
             plugin(iRow).(renameField{iField, 2}) = plugin(iRow).(renameField{iField, 1});
-            rmfield(plugin, renameField{iField, 1});
+            plugin(iRow).(renameField{iField, 1}) = [];
         end
         
         % decode tags
@@ -131,6 +131,7 @@ function [plugin,eeglabVersionStatus] = plugin_getweb(type, pluginOri, varargin)
             end
         end
     end
+    plugin = rmfield(plugin, renameField{iField, 1});
     % handle the special case of EEGLAB version
     indEEGLAB = cellfun(@(x)isequal(x, 'eeglab'), lower( { plugin.name } ));
     eeglabVersionStatus = plugin(indEEGLAB);

commit 1c0b94b2f4459d9e94747fd33b64764fcd81ffb3

bug caused by 895e90ba208b363706d7632402866a57badca993 is resolved

functions/adminfunc/plugin_getweb.m
@@ -131,7 +131,7 @@ function [plugin,eeglabVersionStatus] = plugin_getweb(type, pluginOri, varargin)
             end
         end
     end
-    plugin = rmfield(plugin, renameField{iField, 1});
+    plugin = rmfield(plugin, renameField(:, 1)');
     % handle the special case of EEGLAB version
     indEEGLAB = cellfun(@(x)isequal(x, 'eeglab'), lower( { plugin.name } ));
     eeglabVersionStatus = plugin(indEEGLAB);